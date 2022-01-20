ad439146dd86ad2060d0316e8db8ed985cca1195
Tomasz Marzec <tomasz4.marzec@student.uj.edu.pl>
2022-01-20 14:54:49 +0100

    support for tikz

--- shared.h
+++ shared.h
@@ -2,17 +2,24 @@
 #include <vector>
 #include <cstddef>
 
+enum OutType: unsigned short {
+    normal_t = 1,
+    json_t = 2,
+    tikz_t = 4
+};
+
 class graph {
     public:
-    std::vector<std::pair<int, int>> mem;
+    std::vector<std::pair<int, int>> edges;
     std::vector<std::pair<double,double>> positions;
+    
     size_t size;
 
     graph();
     graph(size_t, const std::vector<std::pair<int, int>>&, const std::vector<std::pair<double,double>>&);
     graph(const graph&);
 
-    void setMem(const std::vector<std::pair<int, int>>&);
+    void setEdges(const std::vector<std::pair<int, int>>&);
     void setPositions(const std::vector<std::pair<double, double>>&);
 
 
